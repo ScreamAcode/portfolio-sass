b65523aff264a0035fd1d10cd553daab5dcab6b8
ScreamAcode <saravanasurya100@gmail.com>
2020-12-17 15:37:49 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,29 @@
+const menuBtn = document.querySelector(".menu-btn");
+const burger = document.querySelector(".menu-btn-burger");
+const nav = document.querySelector(".nav");
+const menuNav = document.querySelector(".menu-nav");
+const menuNavItem = document.querySelectorAll(".menu-nav-item");
+
+let showMenu = false;
+
+menuBtn.addEventListener("click",()=>{
+    if(!showMenu){
+        burger.classList.add("open");
+        nav.classList.add("open");
+        menuNav.classList.add("open");
+        menuNavItem.forEach(item =>{
+            item.classList.add("open")
+        });
+
+        showMenu = true;
+    }else{
+        burger.classList.remove("open");
+        nav.classList.remove("open");
+        menuNav.classList.remove("open");
+        menuNavItem.forEach(item =>{
+            item.classList.remove("open")
+        });
+
+        showMenu = false;
+    }
+})
